ba46f15cf0044a4a79a479156fd53d0e4e9a9b23
FEDRIAN TANWID <s3975596@student.rmit.edu.au>
2025-05-06 20:07:15 +0700

    oke ini yang bener bang

--- Harvest_Moon/src/Tiles/TileManager.java
+++ Harvest_Moon/src/Tiles/TileManager.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import main.GamePanel;
+import Main.GamePanel;
 
 public class TileManager {
     GamePanel gp;
