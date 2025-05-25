c67c5dcc99bd297470cc0849fdabad288b260f33
FEDRIAN TANWID <s3975596@student.rmit.edu.au>
2025-05-25 22:12:12 +0700

    set object sama set npcs

--- Harvest_Moon/src/Main/UI.java
+++ Harvest_Moon/src/Main/UI.java
@@ -295,4 +295,12 @@ public class UI {
         g2.setStroke(new BasicStroke(5));
         g2.drawRoundRect(x + 5, y + 5, width - 10, height - 10, 25, 25);
     }
+
+    public void setObject() {
+        gp.obj.add(new object.OBJ_Papan(gp, 0, 0)); // Example of adding an object
+    }
+
+    public void setNpc() {
+        gp.npcs.add(new NPC_Farmer(gp, 0, 0)); // Example of adding an NPC
+    }
 }
